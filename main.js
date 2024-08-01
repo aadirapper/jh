2d4512ad704fed4b509455bff1beba684fc9bd60
aadirapper <aadirapper8@gmail.com>
2024-08-01 14:23:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function button(){
+
+
+
+    window.open("https://www.w3schools.com")
+
+
+
+}
+function bt(){
+
+
+    window.open("https://tryhackme.com/")
+
+
+
+}
+function but(){
+
+
+
+    window.open("https://youtube.com/playlist?list=PLIhvC56v63IJIujb5cyE13oLuyORZpdkL&si=C12U3qNfdR3nFKvw")
+
+
+
+}
